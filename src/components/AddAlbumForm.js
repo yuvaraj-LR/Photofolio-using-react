e3fab013752759c128caa96fecb70818abe700d1
yuvaraj-LR <yuvarajrama121@gmail.com>
2024-07-20 20:23:48 +0530

    Include Clear functionality

--- src/components/AddAlbumForm.js
+++ src/components/AddAlbumForm.js
@@ -1,19 +1,32 @@
-
+import {useRef, useEffect} from "react";
 
 export default function AddAlbumForm(props) {
 
     const {setalbumList} = props;
-    
+
+    const albumNameInput = useRef(null);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    function handleClear() {
+        albumNameInput.current.value = ""
+    }
+
+    useEffect(() => {
+        albumNameInput.current.focus();
+    }, [])
 
     return (
         <>
             <div className="flex flex_col create_ablum_container">
                 <h2>Create Album</h2>
 
-                <form className="relative">
-                    <input placeholder="Album Name" className="create_album_input" />
+                <form onSubmit={(e) =>handleSubmit(e)} className="relative" >
+                    <input placeholder="Album Name" className="create_album_input" ref={albumNameInput} value={albumNameInput.value}/>
                     
-                    <button className="clear_btn clear_absolute_btn">Clear</button>
+                    <button className="clear_btn clear_absolute_btn" onClick={handleClear}>Clear</button>
                     <button className="flex flex_center create_btn">Create</button>
                 </form>
             </div>  
--- src/components/Photofolio.js
+++ src/components/Photofolio.js
@@ -1,16 +1,23 @@
-import {useState} from "react"
+import {useReducer, useState} from "react"
 
 import AlbumList from "./AlbumList";
 import Navbar from "./Navbar";
 import EmptyBox from "./EmptyBox";
 import AddAlbumForm from "./AddAlbumForm";
 
+function photofolioReducer(state, action) {
+    switch (action.type) {
+        case "ADD":
+
+    }
+}
 
 export default function Photofolio() {
     
-    const [albumList, setalbumList] = useState([]);
+    const [albumList, setalbumList] = useReducer(photofolioReducer, []);
     const [addAlbumStatus, setAddAlbumStatus] = useState(false)
     
+    console.log(albumList.length, "listtt...");
 
     return (
         <>
@@ -29,7 +36,7 @@ export default function Photofolio() {
                     </div>
                     <div className="album_content">
 
-                        {albumList.length < 0 ? <AlbumList albumList={albumList} /> : <EmptyBox text="No Album Found. Please Add To View." />}
+                        {albumList.length > 0 ? <AlbumList albumList={albumList} /> : <EmptyBox text="No Album Found. Please Add To View." />}
                     </div>
                 </div>
                 
